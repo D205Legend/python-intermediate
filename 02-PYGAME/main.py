76106009c763392c636e2f91e98a64e12d81034f
Saharsh Vetcha <saharshvetcha@Kishores-MacBook-Air.local>
2022-05-30 18:05:16 +0530

    30th may classwork

--- 02-PYGAME/main.py
+++ 02-PYGAME/main.py
@@ -7,9 +7,14 @@ pygame.init()
 # Creat the screen 
 screen = pygame.display.set_mode((800, 600))
 
+#Importing player image 
+playerImg = pygame.image.load('player.png')
 
 while True:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             sys.exit()
+
+    screen.blit(playerImg, (100, 100))  
+    pygame.display.update()
